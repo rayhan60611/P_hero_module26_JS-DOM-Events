bee527bc300bd0846cde2f584107187fa2f60c03
rayhan60611 <pecon.alok@gmail.com>
2023-06-16 14:22:39 +0600

    child successfully appanded

--- js/event.js
+++ js/event.js
@@ -1,9 +1,20 @@
 document.getElementById("btn").addEventListener("click", function (event) {
-  const textarea = document.getElementById("textarea").value;
+  const newTextarea = document.getElementById("textarea").value;
   const myInput = document.getElementById("my-input").value;
 
-  const section = document.getElementsByClassName("section");
-  const div = document.createElement("div");
-  div.classList.add("div");
-  hw;
+  const section = document.getElementById("section1");
+
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("div");
+
+  const newH1 = document.createElement("h1");
+  newH1.innerText = myInput;
+
+  const newP = document.createElement("p");
+  newP.innerText = newTextarea;
+
+  //child appending
+  section.appendChild(newDiv);
+  newDiv.appendChild(newH1);
+  newDiv.appendChild(newP);
 });
